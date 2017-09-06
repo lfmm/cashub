126844e148385188e3efa06457f98503eef62727
lfmm <luis.mendes@digidelta-software.com>
2017-09-06 15:57:18 +0100

    ...

---
+++ cashub/Hubs/BHub.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reactive;
+using System.Reactive.Subjects;
+
+namespace cashub.Hubs
+{
+
+    public class BHub : Hub
+    {
+
+        public BHub(): base()
+        {
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Client(Context.ConnectionId).InvokeAsync("Welcome", "Welcome to the b Hub!");
+            report("Client with id " + Context.ConnectionId + " has connected at " + DateTime.Now.ToString());
+            await base.OnConnectedAsync();
+        }
+
+        public async void report(string onWhat)
+        {
+            await Clients.All.InvokeAsync("Report", onWhat);
+        }
+
+    }
+
+}
---
+++ cashub/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace cashub
+{
+    public class Startup
+    {
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // cors listener
+            services.AddCors();
+            // signalr listener
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            // map cors
+            app.UseCors(c => {
+                c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+            });
+            // map signalr
+            app.UseSignalR(c => {
+                c.MapHub<Hubs.BHub>("bhub");
+            });
+        }
+    }
+}
